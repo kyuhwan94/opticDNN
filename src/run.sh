85364391cabf90569c369f75533dfa594e125845
bacarat <47767711+kyuhwan94@users.noreply.github.com>
2025-08-16 19:17:04 +0200

    Delete src directory

--- src/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Loop over a list of configuration files.
-for config in ../configs/*.json; do
-    echo "Running experiment with configuration: $config"
-    python3 train.py --config "$config"
-done
